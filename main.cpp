c904f5f742d69f92a9e67f9c18484ec5e9a15d17
Thomas <medard.thomas@outlook.fr>
2017-05-06 12:26:19 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: Thomas
+ *
+ * Created on 6 mai 2017, 12:25
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+	// initialize resources, if needed
+	// Q_INIT_RESOURCE(resfile);
+
+	QApplication app(argc, argv);
+
+	// create and show your widgets here
+
+	return app.exec();
+}
