84c992a19a7f3fcb2a457c39834348bc6c66ee99
Thomas <medard.thomas@outlook.fr>
2017-05-20 19:26:13 +0200

    Added doc to orders

--- src/order/provideenergyorder.h
+++ src/order/provideenergyorder.h
@@ -1,18 +1,45 @@
 #ifndef PROVIDEENERGYORDER_H
 #define PROVIDEENERGYORDER_H
 
+/**
+ *
+ */
+
 #include "shiporder.h"
 
 class EnergyProvidable;
 
+/**
+ * @brief The ProvideEnergyOrder class. Order to provide energy to a component.
+ *        sub class of [ShipOrder](@ref ShipOrder)
+ */
 class ProvideEnergyOrder: public ShipOrder
 {
 public:
+    /**
+     * @brief ProvideEnergyOrder The constructor.
+     * @param ship The ship wich the order applys to.
+     * @param componentProvided The component to provide energy to.
+     * @param energy The EU quantity to provide.
+     */
     ProvideEnergyOrder(IShip *ship, EnergyProvidable *componentProvided, const int & energy);
+    /**
+     * @brief applyOrder Apply this order. [see ShipOrder](@ref ShipOrder)
+     */
     void applyOrder() override;
+    /**
+     * @brief toString Get the string representation.
+     * @return Get the string representation.
+     */
     std::string toString() override;
 private:
+    /**
+     * @brief energy The EU quantity to provide.
+     */
     int energy;
+    /**
+     * @brief componentProvided The component to provide to.
+     */
     EnergyProvidable *componentProvided;
 };
 
--- src/order/shiporder.h
+++ src/order/shiporder.h
@@ -3,20 +3,53 @@
 
 #include <string>
 
+/**
+ * @file shiporder.h
+ */
+
 class IShip;
 class ShipControl;
 
+/**
+ * @brief The ShipOrder class. An order that can be set to a ship.
+ *        This class is abstract.
+ */
 class ShipOrder
 {
 public:
+    /**
+     * @brief ShipOrder The constructor.
+     * @param ship The ship which order apply to.
+     */
     ShipOrder(IShip *ship);
     virtual ~ShipOrder();
+    /**
+     * @brief applyOrder Apply this order. Virtual
+     */
     virtual void applyOrder() = 0;
+    /**
+     * @brief getControl Get the ship control.
+     * @return The ship control.
+     */
     ShipControl *getControl();
+    /**
+     * @brief toString Get the string representation. Pure virtual
+     * @return Get the string representation.
+     */
     virtual std::string toString() = 0;
+    /**
+     * @brief getShip Get the ship.
+     * @return The ship
+     */
     IShip *getShip();
 private:
+    /**
+     * @brief ship The ship which order applys to
+     */
     IShip *ship;
+    /**
+     * @brief control The ship's control.
+     */
     ShipControl *control;
 };
 
